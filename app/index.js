03391019296af4fb4c95a99918521f4b42fae0b8
phong_lu <phong_lu@symantec.com>
2018-11-27 18:44:47 -0800

    add new dependecies

--- app/index.js
+++ app/index.js
@@ -1,11 +1,41 @@
 'use strict';
+
 const Generator = require('yeoman-generator');
+const mkdirp = require('mkdirp');
+
+const app = 'myapp';
+
 module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts);
-    this.log('Initializing...');
+    
+    //this.log('Initializing...');
+    this.createFolderStructure();
   }
-  start() {
-    this.log('Do something...');
+  
+  createFolderStructure() {
+    var folders = [
+      'src/build/' + app + '/controllers',
+      'src/build/' + app + '/views',
+      'src/build/' + app + '/styles',
+      'src/build/' + app + '/images',
+      'src/build/' + app + '/services'
+    ];
+    folders.forEach(function(folder) {
+      mkdirp(folder);
+    });
   }
+  
+  /*start() {
+    this.prompt([
+      {
+        type    : 'input',
+        name    : 'name',
+        message : 'Enter a name for the new component (i.e.: myNewComponent): '
+      }
+    ]).then( (answers) => {
+      // create destination folder
+      this.destinationRoot(answers.name);
+    });
+  }*/
 };
